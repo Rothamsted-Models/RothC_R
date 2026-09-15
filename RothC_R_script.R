2202633b161d10047a67d5d74dbd6c3d1f0297fc
Jonah Prout <43648800+jprout2@users.noreply.github.com>
2026-09-15 13:00:01 +0100

    Updates input file and main script files
    Most of the changes required for v2.2.0. switch for specifying starting inputs and daily or monthly timestep. Need to add options for where the scenario starts and which level of a loop we are in (e.g. timeFact+1 or just 1).

--- RothC_R_script.R
+++ RothC_R_script.R
@@ -1,8 +1,8 @@
-# RothC R version 2.1.1
+# RothC R version 2.2
 # 
 # Authors: Jonah Prout, Kevin Coleman, and Alice Milne
 #
-# Written in R version 4.2.3 (2023-03-15 ucrt)
+# Written in R version 4.6.0 (2026-04-24 ucrt)
 #
 # The Rothamsted Carbon Model: RothC
 # Developed by David Jenkinson and Kevin Coleman
@@ -214,36 +214,26 @@ RMF_PC <- function(PC){
   
 }
 
+# tells if a year is a leap year
+is_leap_year <- function(year){
+  (year %% 4 == 0 & year %% 100 != 0) | (year %% 400 == 0)
+}
+
 ###############################################################################
 
 # program RothC_R
-
-# set initial pool values (0 to allow spin-up method)
-DPM <- 0.0
-RPM <- 0.0
-Bio <- 0.0
-Hum <- 0.0
-SOC <- 0.0
-
-DPM_Rage <- 0.0
-RPM_Rage <- 0.0
-Bio_Rage <- 0.0
-Hum_Rage <- 0.0
-IOM_Rage <- 50000
-
-# set initial soil moisture deficit
-SMD <- 0.0
-TOC1 <- 0.0
 
 # read in RothC input data file 
 # setwd()
 df_opts <- read.csv('RothC_input.dat',skip = 3, header = 1, nrows = 1, sep = '')
 opt_RMmoist <- df_opts[[1,'opt_RMmoist']]
 opt_SMDbare <- df_opts[[1,'opt_SMDbare']]
-df_head <- read.csv('RothC_input.dat', skip = 6, header = 1, nrows = 1, sep = '')# sep = '' can be removed if file is comma delimited
+opt_tstep <- df_opts[[1,'opt_tstep']]
+opt_init <- df_opts[[1,'opt_init']]
+df_pools <- read.csv('RothC_input.dat', skip = 6, header = 1, nrows = 1, sep = '')
+df_head <- read.csv('RothC_input.dat', skip = 9, header = 1, nrows = 1, sep = '')# sep = '' can be removed if file is comma delimited
 clay <- df_head[[1,'clay']]
 depth <- df_head[[1,'depth']]
-IOM <- df_head[[1,'iom']]
 nsteps <- df_head[[1,'nsteps']]
 if(opt_RMmoist %in% c(2,3)){
   silt <- df_head[[1,'silt']]
@@ -251,202 +241,250 @@ if(opt_RMmoist %in% c(2,3)){
   OC <- df_head[[1,'OC']]
   min_RMmoist <- df_head[[1,'min_RMmoist']]
 }
-df <- read.csv('RothC_input.dat', skip = 9, header = 1, sep = '')# sep = '' can be removed if file is comma delimited
+df <- read.csv('RothC_input.dat', skip = 12, header = 1, sep = '')# sep = '' can be removed if file is comma delimited
 colnames(df) <- c('t_year', 't_month', 't_mod', 't_temp','t_rain','t_evap', 't_Pl_inp', 't_OA_inp', 't_PC', 't_Pl_DPM_f', 't_Pl_RPM_f', 't_OA_DPM_f', 't_OA_RPM_f', 't_OA_Bio_f', 't_OA_Hum_f')
 
-# run RothC to equilibrium using first 12 months of input file df (spin-up)
-k <- 0
-j <- 0
+
+# set initial pool values (supply zeros to active pools (DPM, RPM, Bio, and Hum) for spin-up method)
+IOM <- df_pools[[1,'IOM']]
+DPM <- df_pools[[1,'DPM']]
+RPM <- df_pools[[1,'RPM']]
+Bio <- df_pools[[1,'Bio']]
+Hum <- df_pools[[1,'Hum']]
 
 SOC <- DPM + RPM + Bio + Hum + IOM
 
-timeFact <- 12
+# set inital radiocarbon (50000 for IOM and zeros for active pools for spin-up method)
+IOM_Rage <- df_pools[[1,'IOM_age']]
+DPM_Rage <- df_pools[[1,'DPM_age']]
+RPM_Rage <- df_pools[[1,'RPM_age']]
+Bio_Rage <- df_pools[[1,'Bio_age']]
+Hum_Rage <- df_pools[[1,'Hum_age']]
 
-test = 100.0
-while(test > 0.000001){
-  k <- k + 1
-  j <- j + 1
-  
-  if(k == timeFact+1){
-    k <- 1
-  }
-  
-  TEMP <- df$t_temp[k]
-  RAIN <- df$t_rain[k]
-  PEVAP <- df$t_evap[k]
-  
-  PC <- df$t_PC[k]
-  Pl_DPM_f <- df$t_Pl_DPM_f[k]
-  Pl_RPM_f <- df$t_Pl_RPM_f[k]
-  OA_DPM_f <- df$t_OA_DPM_f[k]
-  OA_RPM_f <- df$t_OA_RPM_f[k]
-  OA_Bio_f <- df$t_OA_Bio_f[k]
-  OA_Hum_f <- df$t_OA_Hum_f[k]
-  
-  Pl_inp <- df$t_Pl_inp[k]
-  
-  OA_inp <- df$t_OA_inp[k]
-  
-  modernC <- df$t_mod[k] / 100.0
-  
-  Total_Rage <- 0.0
-  
-  # calculate RMFs for temperature, moisture, and plant cover
-  RM_Temp <- RMF_Temp(TEMP)
-  if(opt_RMmoist == 1){
-    RM_Moist <- RMF_Moist(RAIN, PEVAP, clay, depth, PC, SMD, opt_RMmoist, opt_SMDbare)
-  } else if(opt_RMmoist %in% c(2,3)){
-    RM_Moist <- RMF_Moist(RAIN, PEVAP, clay, depth, PC, SMD, opt_RMmoist, opt_SMDbare, silt, OC, BulkD, min_RMmoist)
-  }
-  RM_PC <- RMF_PC(PC)
-  
-  # combine RMFs into one.
-  RateM <- RM_Temp*RM_Moist*RM_PC
-  
-  # zero is used when calculating the radiocarbon stage
-  zero <- 0
-  
-  # rate constants are parameters so don't need to be passed
-  DPM_k <- 10.0
-  RPM_k <- 0.3
-  Bio_k <- 0.66
-  Hum_k <- 0.02
-  
-  conr <- log(2)/5568.0
-  
-  tstep <- 1.0/timeFact # monthly 1/12, or daily 1/365
-  
-  exc <- exp(-conr*tstep)
-  
-  # decomposition
-  DPM1 <- DPM * exp(-RateM*DPM_k*tstep)
-  RPM1 <- RPM * exp(-RateM*RPM_k*tstep)
-  Bio1 <- Bio * exp(-RateM*Bio_k*tstep)
-  Hum1 <- Hum * exp(-RateM*Hum_k*tstep)
-  
-  DPM_d <- DPM - DPM1
-  RPM_d <- RPM - RPM1
-  Bio_d <- Bio - Bio1
-  Hum_d <- Hum - Hum1
-  
-  x <- 1.67*(1.85+1.60*exp(-0.0786*clay))
-  
-  # proportion C from each pool into CO2, Bio and Hum
-  DPM_co2 <- DPM_d*(x/(x+1))
-  DPM_Bio <- DPM_d*(0.46/(x+1))
-  DPM_Hum <- DPM_d*(0.54/(x+1))
-  
-  RPM_co2 <- RPM_d*(x/(x+1))
-  RPM_Bio <- RPM_d*(0.46/(x+1))
-  RPM_Hum <- RPM_d*(0.54/(x+1))
-  
-  Bio_co2 <- Bio_d*(x/(x+1))
-  Bio_Bio <- Bio_d*(0.46/(x+1))
-  Bio_Hum <- Bio_d*(0.54/(x+1))
-  
-  Hum_co2 <- Hum_d*(x/(x+1))
-  Hum_Bio <- Hum_d*(0.46/(x+1))
-  Hum_Hum <- Hum_d*(0.54/(x+1))
-  
-  # update C pools
-  DPM2 <- DPM1
-  RPM2 <- RPM1
-  Bio2 <- Bio1 + DPM_Bio + RPM_Bio + Bio_Bio + Hum_Bio
-  Hum2 <- Hum1 + DPM_Hum + RPM_Hum + Bio_Hum + Hum_Hum
-  
-  # split plant C to DPM and RPM
-  Pl_C_DPM <- Pl_inp * Pl_DPM_f
-  Pl_C_RPM <- Pl_inp * Pl_RPM_f
-  
-  # split OA C to DPM, RPM, Bio and Hum
-  OA_C_DPM <- OA_inp * OA_DPM_f
-  OA_C_RPM <- OA_inp * OA_RPM_f
-  OA_C_Bio <- OA_inp * OA_Bio_f
-  OA_C_Hum <- OA_inp * OA_Hum_f
-  
-  # add Plant C and OA_C to DPM, RPM and Hum
-  DPM <- DPM2 + Pl_C_DPM + OA_C_DPM
-  RPM <- RPM2 + Pl_C_RPM + OA_C_RPM
-  Bio <- Bio2 + OA_C_Bio
-  Hum <- Hum2 + OA_C_Hum
-  
-  SOC <- DPM + RPM + Bio + Hum + IOM
-  
-  # calc new ract of each pool
-  DPM_Ract <- DPM1 * exp(-conr*DPM_Rage)
-  RPM_Ract <- RPM1 * exp(-conr*RPM_Rage)
-  
-  Bio_Ract <- Bio1 * exp(-conr*Bio_Rage)
-  DPM_Bio_Ract <- DPM_Bio * exp(-conr*DPM_Rage)
-  RPM_Bio_Ract <- RPM_Bio * exp(-conr*RPM_Rage)
-  Bio_Bio_Ract <- Bio_Bio * exp(-conr*Bio_Rage)
-  Hum_Bio_Ract <- Hum_Bio * exp(-conr*Hum_Rage)
-  
-  Hum_Ract <- Hum1 * exp(-conr*Hum_Rage)
-  DPM_Hum_Ract <- DPM_Hum * exp(-conr*DPM_Rage)
-  RPM_Hum_Ract <- RPM_Hum * exp(-conr*RPM_Rage)
-  Bio_Hum_Ract <- Bio_Hum * exp(-conr*Bio_Rage)
-  Hum_Hum_Ract <- Hum_Hum * exp(-conr*Hum_Rage)
-  
-  IOM_Ract <- IOM * exp(-conr*IOM_Rage)
-  
-  # assign new C from plant and organic amendment the correct age
-  Pl_DPM_Ract <- modernC * Pl_C_DPM
-  Pl_RPM_Ract <- modernC * Pl_C_RPM
-  
-  OA_DPM_Ract <- modernC * OA_C_DPM
-  OA_RPM_Ract <- modernC * OA_C_RPM
-  OA_Hum_Ract <- modernC * OA_C_Hum
-  
-  # update ract for each pool
-  DPM_Ract_new <- Pl_DPM_Ract + OA_DPM_Ract + DPM_Ract*exc
-  RPM_Ract_new <- Pl_RPM_Ract + OA_RPM_Ract + RPM_Ract*exc
-  
-  Bio_Ract_new <- (Bio_Ract + DPM_Bio_Ract + RPM_Bio_Ract + Bio_Bio_Ract + Hum_Bio_Ract)*exc
-  Hum_Ract_new <- (Hum_Ract + DPM_Hum_Ract + RPM_Hum_Ract + Bio_Hum_Ract + Hum_Hum_Ract)*exc
-  
-  Total_Ract <- DPM_Ract_new + RPM_Ract_new + Bio_Ract_new + Hum_Ract_new + IOM_Ract
-  
-  # calculate rage of each pool
-  if(DPM <= zero){
-    DPM_Rage <- zero
-  } else {
-    DPM_Rage <- log(DPM/DPM_Ract_new)/conr
-  }
-  
-  if(RPM <= zero){
-    RPM_Rage <- zero
-  } else {
-    RPM_Rage <- log(RPM/RPM_Ract_new)/conr
-  }
-  
-  if(Bio <= zero){
-    Bio_Rage <- zero
-  } else {
-    Bio_Rage <- log(Bio/Bio_Ract_new)/conr
-  }
-  
-  if(Hum <= zero){
-    Hum_Rage <- zero
-  } else {
-    Hum_Rage <- log(Hum/Hum_Ract_new)/conr
-  }
-  
-  if(SOC <= zero){
-    Total_Rage <- zero
-  } else {
-    Total_Rage <- log(SOC/Total_Ract)/conr
-  }
-  
-  # at the end of each year this checks the stock against previous end of year 
-  if(k %% timeFact == 0){
-    TOC0 <- TOC1
-    TOC1 <- DPM + RPM + Bio + Hum
-    test <- abs(TOC1-TOC0)
-  }
+
+# setting up timeStep variable to reduce the number of places the conditional input needs to be edited
+if(opt_tstep %in% c(1,"m","month","monthly","M","Month","Monthly")){
+  timeStep <- "monthly"
+} else if(opt_tstep %in% c(2,"d","day","daily","D","Day","Daily")){
+  timeStep <- "daily_with_leapyears"
+} else if(opt_tstep %in% c(3)){
+  timeStep <- "daily_without_leap"
 }
 
-Total_Delta <- (exp(-Total_Rage/8035.0) - 1.0) * 1000.0
+# no leap years accounted for in spin-up initialisation
+if(timeStep == "monthly"){
+  timeFact <- 12
+} else if(timeStep %in% c("daily_with_leap","daily_without_leap")){
+  timeFact <- 365
+}
+
+# for defining where the scenario starts (timeFact+1 or 0+1)
+if(opt_init %in% c(1,3)){
+  init_rows <- timeFact
+} else if(opt_init %in% c(2)){
+  init_rows <- 0
+}
+
+# set initial soil moisture deficit
+SMD <- 0.0
+TOC1 <- 0.0
+
+# zero is used when calculating the radiocarbon age
+zero <- 0
+
+# rate constants are parameters so don't need to be passed
+DPM_k <- 10.0
+RPM_k <- 0.3
+Bio_k <- 0.66
+Hum_k <- 0.02
+
+conr <- log(2)/5568.0
+
+tstep <- 1.0/timeFact # monthly 1/12, or daily 1/365
+
+exc <- exp(-conr*tstep)
+
+if(opt_init == "1"){
+  # run RothC to equilibrium using first 12 months of input file df (spin-up)
+  k <- 0
+  j <- 0
+  
+  # START of spin-up
+  test = 100.0
+  while(test > 0.000001){
+    
+    k <- k + 1
+    j <- j + 1
+    
+    if(k == timeFact+1){
+      k <- 1
+    }
+    
+    TEMP <- df$t_temp[k]
+    RAIN <- df$t_rain[k]
+    PEVAP <- df$t_evap[k]
+    
+    PC <- df$t_PC[k]
+    Pl_DPM_f <- df$t_Pl_DPM_f[k]
+    Pl_RPM_f <- df$t_Pl_RPM_f[k]
+    OA_DPM_f <- df$t_OA_DPM_f[k]
+    OA_RPM_f <- df$t_OA_RPM_f[k]
+    OA_Bio_f <- df$t_OA_Bio_f[k]
+    OA_Hum_f <- df$t_OA_Hum_f[k]
+    
+    Pl_inp <- df$t_Pl_inp[k]
+    
+    OA_inp <- df$t_OA_inp[k]
+    
+    modernC <- df$t_mod[k] / 100.0
+    
+    Total_Rage <- 0.0
+    
+    # calculate RMFs for temperature, moisture, and plant cover
+    RM_Temp <- RMF_Temp(TEMP)
+    if(opt_RMmoist == 1){
+      RM_Moist <- RMF_Moist(RAIN, PEVAP, clay, depth, PC, SMD, opt_RMmoist, opt_SMDbare)
+    } else if(opt_RMmoist %in% c(2,3)){
+      RM_Moist <- RMF_Moist(RAIN, PEVAP, clay, depth, PC, SMD, opt_RMmoist, opt_SMDbare, silt, OC, BulkD, min_RMmoist)
+    }
+    RM_PC <- RMF_PC(PC)
+    
+    # combine RMFs into one.
+    RateM <- RM_Temp*RM_Moist*RM_PC
+    
+    # decomposition
+    DPM1 <- DPM * exp(-RateM*DPM_k*tstep)
+    RPM1 <- RPM * exp(-RateM*RPM_k*tstep)
+    Bio1 <- Bio * exp(-RateM*Bio_k*tstep)
+    Hum1 <- Hum * exp(-RateM*Hum_k*tstep)
+    
+    DPM_d <- DPM - DPM1
+    RPM_d <- RPM - RPM1
+    Bio_d <- Bio - Bio1
+    Hum_d <- Hum - Hum1
+    
+    x <- 1.67*(1.85+1.60*exp(-0.0786*clay))
+    
+    # proportion C from each pool into CO2, Bio and Hum
+    DPM_co2 <- DPM_d*(x/(x+1))
+    DPM_Bio <- DPM_d*(0.46/(x+1))
+    DPM_Hum <- DPM_d*(0.54/(x+1))
+    
+    RPM_co2 <- RPM_d*(x/(x+1))
+    RPM_Bio <- RPM_d*(0.46/(x+1))
+    RPM_Hum <- RPM_d*(0.54/(x+1))
+    
+    Bio_co2 <- Bio_d*(x/(x+1))
+    Bio_Bio <- Bio_d*(0.46/(x+1))
+    Bio_Hum <- Bio_d*(0.54/(x+1))
+    
+    Hum_co2 <- Hum_d*(x/(x+1))
+    Hum_Bio <- Hum_d*(0.46/(x+1))
+    Hum_Hum <- Hum_d*(0.54/(x+1))
+    
+    # update C pools
+    DPM2 <- DPM1
+    RPM2 <- RPM1
+    Bio2 <- Bio1 + DPM_Bio + RPM_Bio + Bio_Bio + Hum_Bio
+    Hum2 <- Hum1 + DPM_Hum + RPM_Hum + Bio_Hum + Hum_Hum
+    
+    # split plant C to DPM and RPM
+    Pl_C_DPM <- Pl_inp * Pl_DPM_f
+    Pl_C_RPM <- Pl_inp * Pl_RPM_f
+    
+    # split OA C to DPM, RPM, Bio and Hum
+    OA_C_DPM <- OA_inp * OA_DPM_f
+    OA_C_RPM <- OA_inp * OA_RPM_f
+    OA_C_Bio <- OA_inp * OA_Bio_f
+    OA_C_Hum <- OA_inp * OA_Hum_f
+    
+    # add Plant C and OA_C to DPM, RPM and Hum
+    DPM <- DPM2 + Pl_C_DPM + OA_C_DPM
+    RPM <- RPM2 + Pl_C_RPM + OA_C_RPM
+    Bio <- Bio2 + OA_C_Bio
+    Hum <- Hum2 + OA_C_Hum
+    
+    SOC <- DPM + RPM + Bio + Hum + IOM
+    
+    # calc new ract of each pool
+    DPM_Ract <- DPM1 * exp(-conr*DPM_Rage)
+    RPM_Ract <- RPM1 * exp(-conr*RPM_Rage)
+    
+    Bio_Ract <- Bio1 * exp(-conr*Bio_Rage)
+    DPM_Bio_Ract <- DPM_Bio * exp(-conr*DPM_Rage)
+    RPM_Bio_Ract <- RPM_Bio * exp(-conr*RPM_Rage)
+    Bio_Bio_Ract <- Bio_Bio * exp(-conr*Bio_Rage)
+    Hum_Bio_Ract <- Hum_Bio * exp(-conr*Hum_Rage)
+    
+    Hum_Ract <- Hum1 * exp(-conr*Hum_Rage)
+    DPM_Hum_Ract <- DPM_Hum * exp(-conr*DPM_Rage)
+    RPM_Hum_Ract <- RPM_Hum * exp(-conr*RPM_Rage)
+    Bio_Hum_Ract <- Bio_Hum * exp(-conr*Bio_Rage)
+    Hum_Hum_Ract <- Hum_Hum * exp(-conr*Hum_Rage)
+    
+    IOM_Ract <- IOM * exp(-conr*IOM_Rage)
+    
+    # assign new C from plant and organic amendment the correct age
+    Pl_DPM_Ract <- modernC * Pl_C_DPM
+    Pl_RPM_Ract <- modernC * Pl_C_RPM
+    
+    OA_DPM_Ract <- modernC * OA_C_DPM
+    OA_RPM_Ract <- modernC * OA_C_RPM
+    OA_Hum_Ract <- modernC * OA_C_Hum
+    
+    # update ract for each pool
+    DPM_Ract_new <- Pl_DPM_Ract + OA_DPM_Ract + DPM_Ract*exc
+    RPM_Ract_new <- Pl_RPM_Ract + OA_RPM_Ract + RPM_Ract*exc
+    
+    Bio_Ract_new <- (Bio_Ract + DPM_Bio_Ract + RPM_Bio_Ract + Bio_Bio_Ract + Hum_Bio_Ract)*exc
+    Hum_Ract_new <- (Hum_Ract + DPM_Hum_Ract + RPM_Hum_Ract + Bio_Hum_Ract + Hum_Hum_Ract)*exc
+    
+    Total_Ract <- DPM_Ract_new + RPM_Ract_new + Bio_Ract_new + Hum_Ract_new + IOM_Ract
+    
+    # calculate rage of each pool
+    if(DPM <= zero){
+      DPM_Rage <- zero
+    } else {
+      DPM_Rage <- log(DPM/DPM_Ract_new)/conr
+    }
+    
+    if(RPM <= zero){
+      RPM_Rage <- zero
+    } else {
+      RPM_Rage <- log(RPM/RPM_Ract_new)/conr
+    }
+    
+    if(Bio <= zero){
+      Bio_Rage <- zero
+    } else {
+      Bio_Rage <- log(Bio/Bio_Ract_new)/conr
+    }
+    
+    if(Hum <= zero){
+      Hum_Rage <- zero
+    } else {
+      Hum_Rage <- log(Hum/Hum_Ract_new)/conr
+    }
+    
+    if(SOC <= zero){
+      Total_Rage <- zero
+    } else {
+      Total_Rage <- log(SOC/Total_Ract)/conr
+    }
+    
+    # at the end of each year this checks the stock against previous end of year 
+    if(k %% timeFact == 0){
+      TOC0 <- TOC1
+      TOC1 <- DPM + RPM + Bio + Hum
+      test <- abs(TOC1-TOC0)
+    }
+  }
+  
+  Total_Delta <- (exp(-Total_Rage/8035.0) - 1.0) * 1000.0
+  
+  # END of spin-up
+}
+
 
 co2_tot <- 0
 
@@ -455,9 +493,11 @@ year_list[[1]] <- data.frame(1, j, DPM, RPM, Bio, Hum, IOM, SOC, co2_tot, Total_
 colnames(year_list[[1]]) <- c('Year','Month','DPM_t_C_ha','RPM_t_C_ha','Bio_t_C_ha','Hum_t_C_ha','IOM_t_C_ha','SOC_t_C_ha','CO2_t_C_ha','deltaC')
 
 month_list <- list()
+month_list[[1]] <- data.frame(1,j, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, DPM, RPM, Bio, Hum, IOM, SOC, co2_tot)
+colnames(month_list[[1]]) <- c('Year','Month','Pl_inp_t_C_ha','OA_inp_t_C_ha','TEMP_C','RM_Temp','RAIN_mm','PEVAP_mm','SMD_mm','RM_Moist','PC','RM_PC','DPM_t_C_ha','RPM_t_C_ha','Bio_t_C_ha','Hum_t_C_ha','IOM_t_C_ha','SOC_t_C_ha',"CO2_t_C_ha")
 
-# run RothC after spin-up
-for(i in seq(timeFact+1, nsteps,1)){
+# run RothC after initialisation
+for(i in seq(init_rows+1, nsteps,1)){
   
   TEMP <- df$t_temp[i]
   RAIN <- df$t_rain[i]
@@ -487,6 +527,16 @@ for(i in seq(timeFact+1, nsteps,1)){
   
   # Combine RMFs into one.
   RateM <- RM_Temp*RM_Moist*RM_PC
+  
+  if(timeStep == "daily_with_leap"){ 
+    if(is_leap_year(t_year[i])){
+      timeFact <- 366
+    } else {
+      timeFact <- 365
+    }
+  } 
+  
+  tstep <- 1.0/timeFact
   
   # decomposition
   DPM1 <- DPM * exp(-RateM*DPM_k*tstep)
@@ -614,8 +664,8 @@ for(i in seq(timeFact+1, nsteps,1)){
   Total_Delta <- (exp(-Total_Rage/8035.0) - 1.0)*1000.0
   
   # appending outputs to a list
-  month_list[[i-timeFact]] <- data.frame(df[[i, 't_year']], df[[i,'t_month']],Pl_inp, OA_inp, TEMP, RM_Temp, RAIN, PEVAP, SMD, RM_Moist, PC, RM_PC, DPM, RPM, Bio, Hum, IOM, SOC, co2_tot)
-  colnames(month_list[[i-timeFact]]) = c('Year','Month','Pl_inp_t_C_ha','OA_inp_t_C_ha','TEMP_C','RM_Temp','RAIN_mm','PEVAP_mm','SMD_mm','RM_Moist','PC','RM_PC','DPM_t_C_ha','RPM_t_C_ha','Bio_t_C_ha','Hum_t_C_ha','IOM_t_C_ha','SOC_t_C_ha',"CO2_t_C_ha")
+  month_list[[i+1-timeFact]] <- data.frame(df[[i, 't_year']], df[[i,'t_month']],Pl_inp, OA_inp, TEMP, RM_Temp, RAIN, PEVAP, SMD, RM_Moist, PC, RM_PC, DPM, RPM, Bio, Hum, IOM, SOC, co2_tot)
+  colnames(month_list[[i+1-timeFact]]) = c('Year','Month','Pl_inp_t_C_ha','OA_inp_t_C_ha','TEMP_C','RM_Temp','RAIN_mm','PEVAP_mm','SMD_mm','RM_Moist','PC','RM_PC','DPM_t_C_ha','RPM_t_C_ha','Bio_t_C_ha','Hum_t_C_ha','IOM_t_C_ha','SOC_t_C_ha',"CO2_t_C_ha")
   # appending outputs to end of year_list when loop i equals timeFact
   if(df$t_month[i] == timeFact){
     timeFact_index <- as.integer(i/timeFact)
